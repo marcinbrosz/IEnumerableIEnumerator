bcb4b1727d300ffc042663d3ee4b543b28f38ae7
MarcinB <marcio_br@interia.pl>
2018-01-05 11:59:18 +0100

    Add project files.

---
+++ IEnumerableIEnumerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
